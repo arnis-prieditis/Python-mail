b9f7c8a6a8f1dc128a88597137971d1db860bd8d
arnis-prieditis <110823793+arnis-prieditis@users.noreply.github.com>
2022-08-10 13:23:57 +0300

    Pievienoju failus
    insults.json, kur glabajas ziņas, un send_mail.py - pats sūtīšanas skripts

---
+++ send_mail.py
@@ -0,0 +1,45 @@
+import json #json faila apstradei
+import random
+
+import smtplib, ssl #epasta sutisanai
+
+#mainigie epasta lietam
+port = 465  # For SSL
+
+smtp_server = "smtp.gmail.com"
+sender_email = "vards.uzvards.69420@gmail.com"  # Enter your address
+receiver_email = "arnis.prieditis19@gmail.com"  # Enter receiver address
+
+#atver un ielade sarakstu ar novelejumiem
+f = open('insults.json', 'r')
+insults = json.load(f)
+f.close()
+
+#lai parbauditu, vai ir vel neaizsutiti velejumi
+unused = filter(lambda elem: elem['used'] == 0, insults)
+
+if(len(list(unused)) == 0):
+    print('Vajag jaunus novelejumus')
+else:
+    #dabu poziciju vienam no vel neizmantotajiem
+    pos = random.randint(0, len(insults)-1)
+    while insults[pos]['used'] == 1:
+        pos = random.randint(0, len(insults)-1)
+
+    #ieliek zinas saturu mainigaja
+    saturs = insults[pos]['text']
+    print(saturs)
+
+    #zinas nosutisana ar smtplib un ssl
+    parole = 'wfsfltjdgnrtbtdn' #app password
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, parole)
+        server.sendmail(sender_email, receiver_email, saturs)  
+    
+
+    #nomaina izlietotas zinas statusu ('used':0 -> 'used':1) un ieraksta to json failaa
+    insults[pos]['used'] = 1
+    f = open('insults.json', 'w')
+    json.dump(insults, f)
+    f.close()
